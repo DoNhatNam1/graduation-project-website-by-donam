cfd6ebd22bf3eb537b0926ff3163359373999275
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-07-10 12:00:21 +0700

    testing when not using responsive

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,19 +1,20 @@
 
 
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 import PcHomePageComponent from './_components/Devices/PC/PcHomePageComponent';
 import LaptopHomePageComponent from './_components/Devices/Laptop/LaptopHomePageComponent';
 import IpadHomePageComponent from './_components/Devices/Ipad/IpadHomePageComponent';
-const ResponsiveComponent = dynamic(() => import("./_components/ResponsiveComponent"), { ssr: false })
+// const ResponsiveComponent = dynamic(() => import("./_components/ResponsiveComponent"), { ssr: false })
 
 export default async function Page() {
   return (
     <>
-    <ResponsiveComponent
+    {/* <ResponsiveComponent
       PcHomePageComponent={<PcHomePageComponent/>}
       LaptopHomePageComponent={<LaptopHomePageComponent/>}
       IpadHomePageComponent={<LaptopHomePageComponent/>}
-     />
+     /> */}
+     <PcHomePageComponent />
     </>
   );
 }
