63ee4eed3a7b28d83f1e5e22c6d2a06c9d1f8db7
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-07-15 22:08:16 +0700

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Wait for postgres-v15 and pgadmin-v4 to be ready
-/usr/local/bin/wait-for-it.sh -t 0 postgres-v15:5432 -- echo "postgres-v15 is up"
-/usr/local/bin/wait-for-it.sh -t 0 pgadmin-v4:80 -- echo "pgadmin-v4 is up"
+# /app/graduation-project-website-by-donam/wait-for-it.sh -t 0 postgres-v15:5432 -- echo "postgres-v15 is up"
+# /app/graduation-project-website-by-donam/wait-for-it.sh -t 0 pgadmin-v4:80 -- echo "pgadmin-v4 is up"
 
 # Run Prisma commands
 npx prisma db push --schema=prisma/schema.prisma
