58867a78c50d2d5d46535ecd163ce4e6604caac8
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-07-16 01:55:39 +0700

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,5 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
-# Wait for postgres-v15 and pgadmin-v4 to be ready
-# /app/graduation-project-website-by-donam/wait-for-it.sh -t 0 postgres-v15:5432 -- echo "postgres-v15 is up"
-# /app/graduation-project-website-by-donam/wait-for-it.sh -t 0 pgadmin-v4:80 -- echo "pgadmin-v4 is up"
-
-# Run Prisma commands
-npx prisma db push --schema=prisma/schema.prisma
-npx prisma generate --schema=prisma/schema.prisma
-
-# Run insertData.js
-node /app/graduation-project-website-by-donam/insertData.js
-
-# Start the application
-npm start-docker
+npx prisma generate
+npx prisma db push
+node server.js
