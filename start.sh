0c64f65906e39f85e9b04df5f9b3b95bf6fc7751
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-07-17 20:28:43 +0700

    .

--- start.sh
+++ start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Lệnh để chạy server
 node server.js &
 
-# Đợi một chút để server khởi động
-sleep 5
+sleep 20
 
-# Lệnh để generate Prisma client
 npx prisma generate
 
-# Lệnh để chạy migration với tên là 'init'
 npx prisma migrate dev --name init
 
-# Kết thúc script
 echo "Setup completed."
