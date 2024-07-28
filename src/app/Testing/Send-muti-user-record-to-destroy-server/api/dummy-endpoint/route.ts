07875baf492272f092ba6607c1683c64b548f71e
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-07-28 23:39:43 +0700

    .

--- src/app/Testing/Send-muti-user-record-to-destroy-server/api/dummy-endpoint/route.ts
+++ src/app/Testing/Send-muti-user-record-to-destroy-server/api/dummy-endpoint/route.ts
@@ -1,7 +1,7 @@
 
 import { NextResponse } from 'next/server'
 
-export default function GET() {
+export async function GET() {
     // Bất kỳ xử lý nào ở đây cũng không cần thiết, endpoint này chỉ để nhận request từ load test
     return NextResponse.json({ message: 'Dummy endpoint reached' });
   }
