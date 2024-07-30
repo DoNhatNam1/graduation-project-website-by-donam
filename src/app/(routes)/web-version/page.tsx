05398d8fc6702bbb59c87bc36dec20a8289b1f9f
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-07-30 23:27:18 +0700

    feat: :sparkles: update docker compose and dockerfile and add website-version router

---
+++ src/app/(routes)/web-version/page.tsx
@@ -0,0 +1,20 @@
+
+export default function Page() {
+  return (
+    <>
+      <div className="w-screen h-screen grid place-content-center">
+        {/* <h1 className="text-3xl font-bold text-center text-red-500">
+          Website Alpha
+        </h1> */}
+
+        <h1 className="text-3xl font-bold text-center text-blue-500">
+          Website Beta
+        </h1>
+
+        {/* <h1 className="text-3xl font-bold text-center text-green-500">
+          Website Stable
+        </h1> */}
+      </div>
+    </>
+  );
+}
